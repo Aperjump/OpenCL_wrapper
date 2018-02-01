3c4596091f4aee90f4d967b96deeadd65d3e2943
Aperjump <wangwei_aperion@163.com>
2018-01-31 17:29:14 -0800

    openCL

--- include/openCL_wrapper.cpp
+++ include/openCL_wrapper.cpp
@@ -1,54 +1,170 @@
+
 #include "openCL_wrapper.h"
 
-namespace OpenCL_wrapper {
+namespace ww_matrix {
 	cl_platform_id choose_platform(const std::string& pltfrm_name) {
-		cl_uint platformnum = 0;
-		cl_int err = clGetPlatformIDs(0, NULL, &platformnum);
+		cl_uint platform_num = 0;
+		cl_int err = clGetPlatformIDs(0, NULL, &platform_num);
 		if (err < 0) {
 			perror("query platform error\n");
-			exit(1);
 		}
-		cl_platform_id* platform = (cl_platform_id*)malloc(sizeof(cl_platform_id) * platformnum);
-		cl_int err = clGetPlatformIDs(platformnum, platform, NULL);
+		std::vector<cl_platform_id> platforms(platform_num);
+		err = clGetPlatformIDs(platform_num, &platforms[0], NULL);
 		if (err < 0) {
 			perror("get platform vector error\n");
-			exit(1);
 		}
+		cl_uint index = platform_num;
 		// Query info for each devices and try to match pltfrm_name
-		for (cl_int i = 0; i < platformnum; i++)
+		for (cl_uint i = 0; i < platform_num; i++)
 		{
 			size_t plt_name_len = 0;
-			err = clGetPlatformInfo(platform[i], CL_PLATFORM_NAME, 0, 0, &plt_name_len);
+			err = clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, 0, 0, &plt_name_len);
 			if (err < 0) {
 				perror("cannot find platform\n");
-				exit(1);
 			}
 			std::vector<char> plt_name(plt_name_len);
-			err = clGetPlatformInfo(platform[i], CL_PLATFORM_NAME, plt_name_len,
+			err = clGetPlatformInfo(platforms[i], CL_PLATFORM_NAME, plt_name_len,
 				&plt_name[0], NULL);
 			if (err < 0) {
 				perror("cannot find platform\n");
-				exit(1);
 			}
 			if (std::string(&plt_name[0]).find(pltfrm_name) != std::string::npos)
 			{
-				cl_platform_id ret = platform[i];
-				free(platform);
-				return ret;
+				printf("%s\n", &plt_name[0]);
+				index = i;
 			}
 		}
+		if (index >= platform_num) {
+			perror("out of platform index\n");
+			exit(1);
+		}
 		// may have truble
-		return NULL;
+		return platforms[index];
 	}
-	std::vector<cl_device_id> choose_mult_device(cl_platform_id, std::string& device_type) {
-
+	std::vector<cl_device_id> choose_mult_device(cl_platform_id platform, const std::string& device_type) {
+		cl_device_type type;
+		if (device_type == "gpu" || device_type == "GPU") {
+			type = CL_DEVICE_TYPE_GPU;
+		} 
+		else if (device_type == "cpu" || device_type == "CPU") {
+			type = CL_DEVICE_TYPE_CPU;
+		} 
+		else {
+			type = CL_DEVICE_TYPE_DEFAULT;
+		}
+		cl_uint device_num = 0;
+		cl_int err = clGetDeviceIDs(platform, type, 0, 0, &device_num);
+		if (err < 0) {
+			perror("cannot find device");
+		}
+		std::vector<cl_device_id> devices(device_num);
+		err = clGetDeviceIDs(platform, type, device_num, &devices[0], NULL);
+		if (err < 0) {
+			perror("cannot find device");
+		}
+		return devices;
 	}
-	cl_device_id choose_device(cl_platform_id platform, std::string& devs_name, int offset) {
-
+	cl_device_id choose_device(cl_platform_id platform, const std::string& devs_name, size_t offset) {
+		std::vector<cl_device_id> devices = choose_mult_device(platform, devs_name);
+		assert(offset < devices.size());
+		return devices[offset];
 	}
-
+	CL_Base& CL_Base::operator= (const CL_Base& other) {
+		_platform = other._platform;
+		current_device = other.current_device;
+		_devices = other._devices;
+		_context = other._context;
+		_cmdque = other._cmdque;
+		_program = other._program;
+		return *this;
+	}
 	bool CL_Base::init(const std::string& device_type, cl_command_queue_properties queue_prop = 0,
 		const cl_context_properties* additional_context_props = 0) {
-
+		cl_int err = 0;
+		_devices = ww_matrix::choose_mult_device(_platform, device_type);
+		current_device = _devices[0];
+		char deviceName[1024];
+		clGetDeviceInfo(current_device, CL_DEVICE_NAME, 1024, deviceName, NULL);
+		printf("%s\n", deviceName);
+		_context = clCreateContext(NULL, 1, &current_device, NULL, NULL, &err);
+		if (err < 0) {
+			perror("create context error\n");
+			return false;
+		}
+		_cmdque = clCreateCommandQueue(_context, current_device, NULL, &err);
+		if (err < 0) {
+			perror("create command queue error\n");
+			return false;
+		}
+		return true;
 	}
+	cl_device_id CL_Base::switch_device(int offset) {
+		current_device = _devices[offset]; 
+		cl_int err = 0;
+		_context = clCreateContext(NULL, 1, &current_device, NULL, NULL, &err);
+		if (err < 0) {
+			perror("create context error\n");
+		}
+		_cmdque = clCreateCommandQueue(_context, current_device, NULL, &err);
+		if (err < 0) {
+			perror("create command queue error\n");
+		}
+		return current_device; 
+	}
+	bool CL_Base::create_program(const std::string& file) {
+		std::fstream kernelfile(file.c_str(), std::fstream::in | std::fstream::out);
+		std::string content((std::istreambuf_iterator<char>(kernelfile)),
+			std::istreambuf_iterator<char>());
+		size_t program_size = content.size();
+		const char * kernel = (char*)malloc(content.size());
+		kernel = content.c_str();
+		cl_int err = 0;
+		_program = clCreateProgramWithSource(_context, 1, (const char**)&kernel,
+			&program_size, &err);
+		if (err < 0) {
+			perror("create program error");
+			return false;
+		}
+		size_t log_size = 0;
+		char* program_log;
+		err = clBuildProgram(_program, 0, NULL, NULL, NULL, NULL);
+		if (err < 0) {
+			/* Find size of log and print to std output */
+			clGetProgramBuildInfo(_program, current_device, CL_PROGRAM_BUILD_LOG,
+				0, NULL, &log_size);
+			program_log = (char*)malloc(log_size + 1);
+			program_log[log_size] = '\0';
+			clGetProgramBuildInfo(_program, current_device, CL_PROGRAM_BUILD_LOG,
+				log_size + 1, program_log, NULL);
+			printf("%s\n", program_log);
+			free(program_log);
+			exit(1);
+		}
+		clGetProgramBuildInfo(_program, current_device, CL_PROGRAM_BUILD_LOG,
+			0, NULL, &log_size);
+		program_log = (char*)malloc(log_size + 1);
+		program_log[log_size] = '\0';
+		clGetProgramBuildInfo(_program, current_device, CL_PROGRAM_BUILD_LOG,
+			log_size + 1, program_log, NULL);
+		printf("%s\n", program_log);
+		free(program_log);
+		return true;
+	}
+	//bool CL_Base::create_kernels() {
+	//	kernel_num = 0;
+	//	cl_int err = 0;
+	//	err = clCreateKernelsInProgram(_program, 0, NULL, &kernel_num);
+	//	if (err < 0) {
+	//		perror("cannot find how many kernels\n");
+	//		return false;
+	//	}
+	//	_kernels = (cl_kernel*)malloc(sizeof(cl_kernel) * kernel_num);
+	//	err = clCreateKernelsInProgram(_program, kernel_num, _kernels, NULL);
+	//	if (err < 0) {
+	//		perror("cannot create kernels\n");
+	//		return false;
+	//	}
+	//	return true;
+	//}
+
 };
--- include/openCL_wrapper.h
+++ include/openCL_wrapper.h
@@ -3,31 +3,38 @@
 #include <cstdlib>
 #include <string>
 #include <vector>
-namespace OpenCL_wrapper {
+#include <fstream>
+#include <cassert>
+namespace ww_matrix {
 	// choose platform given platform name 
 	cl_platform_id choose_platform(const std::string& pltfrm_name);
 	// offset is device's position in a vector<cl_device_id>
 	// this function will call choose_mult_device and select one with offset
-	cl_device_id choose_device(cl_platform_id platform, std::string& devs_name, int offset);
-	std::vector<cl_device_id> choose_mult_device(cl_platform_id, std::string& device_type);
+	cl_device_id choose_device(cl_platform_id platform, const std::string& devs_name, size_t offset);
+	std::vector<cl_device_id> choose_mult_device(cl_platform_id, const std::string& device_type);
 	// contains all required structure for computation
 	class CL_Base {
 	public:
-		CL_Base(const std::string& pltform_name) { _platform = OpenCL_wrapper::choose_platform(pltform_name); }
+		CL_Base(const std::string& pltform_name) { _platform = ww_matrix::choose_platform(pltform_name); }
 		CL_Base(cl_platform_id platform) : _platform(platform) { }
-		~CL_Base();
-		bool init(const std::string& device_type, cl_command_queue_properties queue_prop = 0, 
-			const cl_context_properties* additional_context_props = 0);
-		cl_device_id switch_device(int offset) { current_device = _devices[offset]; return current_device; }
-		cl_platform_id get_platform() { return _platform;  }
-		cl_device_id get_device() { return current_device; }
-		cl_context get_context() { return _context; }
-		cl_command_queue get_cmdque() { return _cmdque; }
-	private:
+		CL_Base(CL_Base& other) : _platform(other._platform),
+			current_device(other.current_device), _devices(other._devices),
+			_context(other._context), _cmdque(other._cmdque), _program(other._program) { }
+		CL_Base& operator= (const CL_Base& other);
+		~CL_Base() { }
+		bool init(const std::string& device_type, cl_command_queue_properties queue_prop, 
+			const cl_context_properties* additional_context_props);
+		cl_device_id switch_device(int offset);
+		bool create_program(const std::string& file);
+		//bool create_kernels();
+
 		cl_platform_id _platform;
 		cl_device_id current_device;
 		std::vector<cl_device_id> _devices;
 		cl_context _context;
 		cl_command_queue _cmdque;
+		cl_program _program;
+		//cl_kernel* _kernels;
+		//cl_uint kernel_num;
 	};
 };
